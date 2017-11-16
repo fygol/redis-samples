560237c86c4efe2801d99ab9fa2828035cd800e3
John Smith <ecco_fatto@mail.ru>
2017-11-16 17:24:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Redis Samples
+
+Run redis server in docker container 
+> $ docker-compose up -d
+
+Run redis-cli inside redis server container
+> docker exec -it redis-server redis-cli
---
+++ src/test/java/io/samples/redis/StringTest.java
@@ -0,0 +1,28 @@
+package io.samples.redis;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    Jedis redis;
+
+    @Before
+    public void setUp() throws Exception {
+        redis = new Jedis("localhost", 6379);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        redis.close();
+    }
+
+    @Test
+    public void set_and_get() throws Exception {
+        redis.set("key1", "value1");
+        assertThat(redis.get("key1")).isEqualTo("value1");
+    }
+}
